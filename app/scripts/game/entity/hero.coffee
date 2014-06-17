a465be2011a3af1a4394f69fedad9613defa28db
Andrew Jones <acjones617@gmail.com>
2014-06-16 18:43:10 -0700

    Merge Andrew's front end changes

--- app/scripts/game/entity/hero.coffee
+++ app/scripts/game/entity/hero.coffee
@@ -1,4 +1,3 @@
-
 define( ->
   fontStyle = { font: "20px Arial", fill: "#ffffff", align: "left" }
 
@@ -9,22 +8,25 @@ define( ->
 
     constructor: (@game, @phaser, @meta) ->
       @sprite = null
+      @speed = 400
+      @startOnScreenPos = 10
 
       @upKey = null
       @downKey = null
       @leftKey = null
       @rightKey = null
-
+      
     preload: ->
       @game.load.spritesheet "roshan", "images/roshan.png", 32, 48
 
     create: ->
       @sprite = @game.add.sprite(250, 250, "roshan")
       @game.physics.enable(@sprite, @phaser.Physics.ARCADE)
+      @sprite.body.collideWorldBounds = true;
       @sprite.body.bounce.set(1)
-      @expText = @game.add.text(20, 10, "exp: #{@meta.exp}", fontStyle)
-      @healthText = @game.add.text(20, 30, "health: #{@meta.health}", fontStyle)
-      @mana = @game.add.text(20, 50, "mana: #{@meta.mana}", fontStyle)
+      expText = @game.add.text(20, 10, "exp: #{@meta.exp}", fontStyle)
+      healthText = @game.add.text(20, 30, "health: #{@meta.health}", fontStyle)
+      mana = @game.add.text(20, 50, "mana: #{@meta.mana}", fontStyle)
      
       @sprite.animations.add "down", [0, 3], false
       @sprite.animations.add "left", [4, 7], false
@@ -33,33 +35,36 @@ define( ->
       @_setControls()
 
     update: ->
-      if @sprite.x is 0
-        @sprite.x = @game.width - 20
+      @sprite.body.velocity.x = 0
+      @sprite.body.velocity.y = 0
+
+      if @sprite.x < 0
+        @sprite.x = @game.width - @startOnScreenPos
         @trigger('changeMap', 'left')
 
-      if @sprite.x is @game.width
-        @sprite.x = 10
+      if @sprite.x > @game.width
+        @sprite.x = @startOnScreenPos
         @trigger('changeMap', 'right')
 
-      if @sprite.y is 0
-        @sprite.y = @game.height - 20
+      if @sprite.y < 0
+        @sprite.y = @game.height - @startOnScreenPos
         @trigger('changeMap', 'up')
 
-      if @sprite.y is @game.height
-        @sprite.y = 10
+      if @sprite.y > @game.height
+        @sprite.y = @startOnScreenPos
         @trigger('changeMap', 'down')
 
       if @upKey.isDown
-        @sprite.y -= 2
+        @sprite.body.velocity.y = -@speed
         @sprite.animations.play "up", 5, false
       else if @downKey.isDown
-        @sprite.y += 2
+        @sprite.body.velocity.y = @speed
         @sprite.animations.play "down", 5, false
-      if @leftKey.isDown
-        @sprite.x -= 2
+      else if @leftKey.isDown
+        @sprite.body.velocity.x = -@speed
         @sprite.animations.play "left", 5, false
       else if @rightKey.isDown
-        @sprite.x += 2
+        @sprite.body.velocity.x = @speed
         @sprite.animations.play "right", 5, false
 
       @sprite.bringToTop()
@@ -71,10 +76,6 @@ define( ->
       @downKey = @game.input.keyboard.addKey(Phaser.Keyboard.DOWN)
       @leftKey = @game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
       @rightKey = @game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
-
-    takeDmg: (hero, enemy) ->
-      @meta.health -= 10
-      @healthText.text = "health: #{@meta.health}"
 
   return Player
 )
--- app/scripts/game/entity/hero.js
+++ app/scripts/game/entity/hero.js
@@ -20,6 +20,8 @@
         this.phaser = phaser;
         this.meta = meta;
         this.sprite = null;
+        this.speed = 400;
+        this.startOnScreenPos = 10;
         this.upKey = null;
         this.downKey = null;
         this.leftKey = null;
@@ -31,12 +33,14 @@
       };
 
       Player.prototype.create = function() {
+        var mana;
         this.sprite = this.game.add.sprite(250, 250, "roshan");
         this.game.physics.enable(this.sprite, this.phaser.Physics.ARCADE);
+        this.sprite.body.collideWorldBounds = true;
         this.sprite.body.bounce.set(1);
-        this.expText = this.game.add.text(20, 10, "exp: " + this.meta.exp, fontStyle);
-        this.healthText = this.game.add.text(20, 30, "health: " + this.meta.health, fontStyle);
-        this.mana = this.game.add.text(20, 50, "mana: " + this.meta.mana, fontStyle);
+        expText = this.game.add.text(20, 10, "exp: " + this.meta.exp, fontStyle);
+        healthText = this.game.add.text(20, 30, "health: " + this.meta.health, fontStyle);
+        mana = this.game.add.text(20, 50, "mana: " + this.meta.mana, fontStyle);
         this.sprite.animations.add("down", [0, 3], false);
         this.sprite.animations.add("left", [4, 7], false);
         this.sprite.animations.add("right", [8, 11], false);
@@ -45,34 +49,35 @@
       };
 
       Player.prototype.update = function() {
-        if (this.sprite.x === 0) {
-          this.sprite.x = this.game.width - 20;
+        this.sprite.body.velocity.x = 0;
+        this.sprite.body.velocity.y = 0;
+        if (this.sprite.x < 0) {
+          this.sprite.x = this.game.width - this.startOnScreenPos;
           this.trigger('changeMap', 'left');
         }
-        if (this.sprite.x === this.game.width) {
-          this.sprite.x = 10;
+        if (this.sprite.x > this.game.width) {
+          this.sprite.x = this.startOnScreenPos;
           this.trigger('changeMap', 'right');
         }
-        if (this.sprite.y === 0) {
-          this.sprite.y = this.game.height - 20;
+        if (this.sprite.y < 0) {
+          this.sprite.y = this.game.height - this.startOnScreenPos;
           this.trigger('changeMap', 'up');
         }
-        if (this.sprite.y === this.game.height) {
-          this.sprite.y = 10;
+        if (this.sprite.y > this.game.height) {
+          this.sprite.y = this.startOnScreenPos;
           this.trigger('changeMap', 'down');
         }
         if (this.upKey.isDown) {
-          this.sprite.y -= 2;
+          this.sprite.body.velocity.y = -this.speed;
           this.sprite.animations.play("up", 5, false);
         } else if (this.downKey.isDown) {
-          this.sprite.y += 2;
+          this.sprite.body.velocity.y = this.speed;
           this.sprite.animations.play("down", 5, false);
-        }
-        if (this.leftKey.isDown) {
-          this.sprite.x -= 2;
+        } else if (this.leftKey.isDown) {
+          this.sprite.body.velocity.x = -this.speed;
           this.sprite.animations.play("left", 5, false);
         } else if (this.rightKey.isDown) {
-          this.sprite.x += 2;
+          this.sprite.body.velocity.x = this.speed;
           this.sprite.animations.play("right", 5, false);
         }
         this.sprite.bringToTop();
@@ -83,11 +88,6 @@
         this.downKey = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
         this.leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
         return this.rightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-      };
-
-      Player.prototype.takeDmg = function(hero, enemy) {
-        this.meta.health -= 10;
-        return this.healthText.text = "health: " + this.meta.health;
       };
 
       return Player;
