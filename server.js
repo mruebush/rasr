e9406a0b330df87ec30b4950e8256131e8c0e5d1
Sean Dokko <dokko1230@gmail.com>
2014-06-23 14:06:43 -0700

    adds revisited grunt file and moves images to assets

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var path = require('path');
 app.use('/assets', express.static(path.resolve(__dirname + '/public/assets')));
 app.use('/bower_components', express.static(path.resolve(__dirname + '/public/bower_components')));
 app.use('/scripts', express.static(path.resolve(__dirname + '/public/scripts')));
+app.use('/styles', express.static(path.resolve(__dirname + '/public/styles')));
 app.use('/mocks', express.static(path.resolve(__dirname + '/public/mocks')));
 app.use('/node_modules', express.static(path.resolve(__dirname + '/node_modules')));
 
