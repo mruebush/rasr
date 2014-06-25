59e357b6e2d22b2e2d19ab2858d9d2974a0e64ef
Andrew Jones <acjones617@gmail.com>
2014-06-25 11:31:11 -0700

    Angularize player

--- public/js/game/services/entity/PlayerModel.js
+++ public/js/game/services/entity/PlayerModel.js
@@ -1,49 +1,52 @@
 (function() {
-  app.service('Player', function() {
+  app.factory('Player', function() {
+    var Player;
+    Player = {};
     return function(game, phaser, meta) {
-      this.game = game;
-      this.phaser = phaser;
-      this.meta = meta;
-      this.sprite = null;
-      this.user = null;
-      this.preload = function() {
-        this.game.load.atlasXML("player", "images/player.png", "images/player.xml");
-        return this.game.load.image('arrow', 'images/bullet.png');
+      Player.game = game;
+      Player.phaser = phaser;
+      Player.meta = meta;
+      Player.sprite = null;
+      Player.user = null;
+      Player.preload = function() {
+        Player.game.load.atlasXML("player", "assets/player.png", "assets/player.xml");
+        return Player.game.load.image('arrow', 'assets/bullet.png');
       };
-      this.create = function() {
-        this.sprite = this.game.add.sprite(this.meta.x, this.meta.y, "player");
-        this.game.physics.enable(this.sprite, this.phaser.Physics.ARCADE);
-        this.sprite.body.collideWorldBounds = true;
-        this.sprite.body.bounce.set(1);
-        this.sprite.animations.add("down", Phaser.Animation.generateFrameNames('player_walk_down', 0, 11, '.png', 4), 30, false);
-        this.sprite.animations.add("left", Phaser.Animation.generateFrameNames('player_walk_left', 0, 11, '.png', 4), 30, false);
-        this.sprite.animations.add("right", Phaser.Animation.generateFrameNames('player_walk_right', 0, 11, '.png', 4), 30, false);
-        this.sprite.animations.add("up", Phaser.Animation.generateFrameNames('player_walk_up', 0, 11, '.png', 4), 30, false);
-        this.sprite.animations.add("attack_down", Phaser.Animation.generateFrameNames('player_attack_down', 0, 4, '.png', 4), 15, false);
-        this.sprite.animations.add("attack_left", Phaser.Animation.generateFrameNames('player_attack_left', 0, 4, '.png', 4), 15, false);
-        this.sprite.animations.add("attack_right", Phaser.Animation.generateFrameNames('player_attack_right', 0, 4, '.png', 4), 15, false);
-        return this.sprite.animations.add("attack_up", Phaser.Animation.generateFrameNames('player_attack_up', 0, 4, '.png', 4), 15, false);
+      Player.create = function() {
+        Player.sprite = Player.game.add.sprite(Player.meta.x, Player.meta.y, "player");
+        Player.game.physics.enable(Player.sprite, Player.phaser.Physics.ARCADE);
+        Player.sprite.body.collideWorldBounds = true;
+        Player.sprite.body.bounce.set(1);
+        Player.sprite.animations.add("down", Phaser.Animation.generateFrameNames('player_walk_down', 0, 11, '.png', 4), 30, false);
+        Player.sprite.animations.add("left", Phaser.Animation.generateFrameNames('player_walk_left', 0, 11, '.png', 4), 30, false);
+        Player.sprite.animations.add("right", Phaser.Animation.generateFrameNames('player_walk_right', 0, 11, '.png', 4), 30, false);
+        Player.sprite.animations.add("up", Phaser.Animation.generateFrameNames('player_walk_up', 0, 11, '.png', 4), 30, false);
+        Player.sprite.animations.add("attack_down", Phaser.Animation.generateFrameNames('player_attack_down', 0, 4, '.png', 4), 15, false);
+        Player.sprite.animations.add("attack_left", Phaser.Animation.generateFrameNames('player_attack_left', 0, 4, '.png', 4), 15, false);
+        Player.sprite.animations.add("attack_right", Phaser.Animation.generateFrameNames('player_attack_right', 0, 4, '.png', 4), 15, false);
+        return Player.sprite.animations.add("attack_up", Phaser.Animation.generateFrameNames('player_attack_up', 0, 4, '.png', 4), 15, false);
       };
-      this.move = function(data) {
+      Player.move = function(data) {
         var dir;
         dir = data.dir;
-        this.sprite.y = data.y;
-        this.sprite.x = data.x;
+        Player.sprite.y = data.y;
+        Player.sprite.x = data.x;
         if (dir === 'up') {
-          this.sprite.animations.play("up", 5, false);
+          Player.sprite.animations.play("up", 5, false);
         } else if (dir === 'down') {
-          this.sprite.animations.play("down", 5, false);
+          Player.sprite.animations.play("down", 5, false);
         }
         if (dir === 'left') {
-          this.sprite.animations.play("left", 5, false);
+          Player.sprite.animations.play("left", 5, false);
         } else if (dir === 'right') {
-          this.sprite.animations.play("right", 5, false);
+          Player.sprite.animations.play("right", 5, false);
         }
-        return this.update();
+        return Player.update();
       };
-      return this.update = function() {
-        this.sprite.bringToTop();
+      Player.update = function() {
+        Player.sprite.bringToTop();
       };
+      return Player;
     };
   });
 
